f04ba87a2805924f978f8a6040ccaa4cf7d2b7d8
haejupark <phj0225@yonsei.ac.kr>
2019-01-16 20:47:43 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,18 +1,20 @@
 import sys
 
-word_embed_size = 300
-max_len_sentence = 50
-lambda1 = 0.05
-
-cnli_train_path = "data/cnli/cnli_train_processed.txt"
-cnli_dev_path = "data/cnli/cnli_dev_processed.txt"
-#cnli_test_path = "data/cnli/cnli_test_processed.txt"
-
-snli_train_path = "data/snli/train_processed.txt"
-snli_dev_path = "data/snli/dev_processed.txt"
-
-embeddings_chinese = "data/sgns.merge.word" 
-embeddings_english = "data/wiki.en.vec"
+class Setting(object):
+	def __init__(self):
+		self.lr = 0.001
+		self.word_dim = 300
+		self.lstm_dim = 256
+		self.max_len = 30
+		self.dense_dim =  128
+		self.keep_prob = 0.6
+		self.batch_size = 1024
+		self.epochs = 50
+		self.lambda1 = 0.005
+		self.train_dir = "data/xnli-mt/multinli/"
+		self.dev_dir = "data/xnli/"
+		self.embed_dir = "../aaai2019/data/"
 
 labels = {'neutral':0, 'entailment':1, 'contradiction':2}
 labels_reverse = {0:'neutral', 1:'entailment', 2:'contradiction'}
+
